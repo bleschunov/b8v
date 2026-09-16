eed6969cfcbc7493ddf0245bddd2ca00e898bf87
bleschunov <d.bleshchunov@actum.cx>
2026-09-16 12:56:37 +0200

    trigger infra ci/cd

--- infra/layer.tf
+++ infra/layer.tf
@@ -1,26 +1,4 @@
-resource "terraform_data" "build_layer" {
-  triggers_replace = [
-    filemd5("${path.module}/../api/requirements.txt"),
-    "v4"
-  ]
-
-  provisioner "local-exec" {
-    command = <<-EOT
-      rm -rf ${path.module}/../dist/layer_build
-      mkdir -p ${path.module}/../dist/layer_build/python
-
-      pip3 install \
-        -r ${path.module}/../api/requirements.txt \
-        -t ${path.module}/../dist/layer_build/python \
-        --platform manylinux2014_x86_64 \
-        --only-binary=:all:
-    EOT
-  }
-}
-
 data "archive_file" "layer" {
-  depends_on = [terraform_data.build_layer]
-
   type        = "zip"
   source_dir  = "${path.module}/../dist/layer_build"
   output_path = "${path.module}/../dist/layer.zip"
